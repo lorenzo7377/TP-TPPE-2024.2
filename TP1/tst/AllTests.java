5b01189e4d4ad509a572583e6bb02d776727347a
Eduardo-RFarias <eduardo.rfarias@outlook.com>
2025-01-22 22:52:33 -0300

    Revert "deleção de pasta repetida"
    This reverts commit c55f7a465229be3a8d2d032ba5c021324edae59d.

---
+++ TP1/tst/AllTests.java
@@ -0,0 +1,21 @@
+package tst;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+		TesteCadastrarDependente.class,
+		TesteRendimentos.class,
+		TesteCalculosDeducoesDependentes.class,
+		TesteCadastroContribuicaoPrevidenciaria.class,
+		TesteCadastroPensaoAlimenticia.class,
+		TesteCadastroOutrasDeducoes.class,
+		TesteCalcularAliquotaEfetiva.class,
+		TesteBaseDeCalculo.class,
+		TesteCalcularImposto.class,
+})
+public class AllTests {
+
+}
